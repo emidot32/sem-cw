7d47990036ea3b2a047d83f82c7a320bae8d4bba
Illia Komisarov <emidot32@gmail.com>
2022-06-10 20:49:12 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='sem-cw',
+    version='0.0.1',
+    packages=[''],
+    url='htts://127.0.01:5000',
+    license='',
+    author='Illia Komisarov',
+    author_email='emidot32@gmail.com',
+    description=''
+)
